fd7269afee6eabd357f9290587ef0daffd865ed0
evgenybinkovskyi <binkovskiievgenii94@gmail.com>
2024-07-13 20:37:26 +0200

    feat: implement functional burger-menu, add style them

--- js/script.js
+++ js/script.js
@@ -21,3 +21,14 @@ if (menuLinks.length > 0) {
         }
     }
 }
+
+//burger-menu
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
